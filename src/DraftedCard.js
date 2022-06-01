22009a6aa50e39760f37201d2b9f5f0830e77883
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-01 14:13:40 -0700

    deleting unused variables per Netlify deploy failure

--- src/DraftedCard.js
+++ src/DraftedCard.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { deleteDraftedCard } from './services/supabase-utils';
-import { useState } from 'react';
-import LoadingSpinner from './LoadingSpinner';
 
 export default function DraftedCard({ imageUrl, id, setDeleteCard }) {
-  const [isLoading, setIsLoading] = useState(false);
 
   async function handleClick() {
-    setIsLoading(true);
     await deleteDraftedCard(id);
 
     setDeleteCard(id);
-    setIsLoading(false);
   }
 
   return (
