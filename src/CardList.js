56a29f1aad54138060c1a215b1b04df18d9e5bf8
THOMAS HARDING <hardingt604@gmail.com>
2022-06-02 16:50:50 -0600

    add more comment pseudocode to files

--- src/CardList.js
+++ src/CardList.js
@@ -25,8 +25,10 @@ export default function CardList({
   return (
     <span>
       <div className="draft-container">
+        {/* CardList is all cards on the screen, split into the top "search" part and the bottom "drafted" part */}
         <h1>Choose Cards</h1>
         <div className="card-list">
+          {/* Each card fetched from the API is a Card component with an on click function to draft it */}
           {cards.map(({ name, imageUrl, multiverseid }) => (
             <Card
               key={name + multiverseid}
@@ -38,10 +40,12 @@ export default function CardList({
             />
           ))}
         </div>
+        {/* We don't need to show the drafted cards if there aren't any of them */}
         {drafted.length > 0 ? (
           <div>
             <h1>{deckName} Draft</h1>
             <div className="draft-list">
+              {/* Each card already drafted into supabase is a Card component with an on click function to remove it from the deck */}
               {drafted.map(({ name, imageUrl, id }) => (
                 <DraftedCard
                   key={name + id}
--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Image from './images/magic-card-back.png';
 
-export default function DecksList({ deck_name, id }) {
+export default function Deck({ deck_name, id }) {
   return (
+    // Component is a view of a single deck, represented by a card back and its name
     <Link to={`/deck/${id}`}>
       <div className="deck">
         <h3>{deck_name}</h3>
